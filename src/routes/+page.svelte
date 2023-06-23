77204f102a073a76d119323e77b6e4c2b98ba107
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2023-06-23 20:56:01 +0100

    add bg to wrapper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="grid h-full place-items-center">
-	<div class="">
+	<div class="bg-base-100">
 		<div class="border-4 border-primary-content p-2">
 			<div class="bg-primary-content px-4 py-3 text-4xl font-extrabold uppercase text-base-100">
 				Lee Biddiscombe
