476bf4d36d1fc6e365c4ecb2649d0e81df6994bf
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2023-07-18 18:03:33 +0100

    trying build

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,22 +3,21 @@
 	import Card from './card.svelte';
 	export let data;
 
-	let searchTags: Set<string>  = new Set()
+	let searchTags: Set<string> = new Set();
 
 	function handleTagClick(tag: string) {
-		if (!searchTags.has(tag))	{
-			searchTags.add(tag)
+		if (!searchTags.has(tag)) {
+			searchTags.add(tag);
 		} else {
-			searchTags.delete(tag)
+			searchTags.delete(tag);
 		}
 
-		searchTags = searchTags
+		searchTags = searchTags;
 	}
 
-	$: content = data.content.filter((post: Content) => searchTags.size === 0  ||
-			[...searchTags].every(tag => post.tags.includes(tag))
-		)
-
+	$: content = data.content.filter(
+		(c: Content) => searchTags.size === 0 || [...searchTags].every((tag) => c.tags.includes(tag))
+	);
 </script>
 
 <svelte:head>
@@ -28,11 +27,16 @@
 
 <div class="flex gap-2 py-2">
 	{#each data.tags as tag}
-		<button class="rounded bg-base-200 px-2 py-1 font-mono text-xs" class:outline = {searchTags.has(tag)} value={tag} on:click={() => handleTagClick(tag)}>&num;{tag}</button>
+		<button
+			class="rounded bg-base-200 px-2 py-1 font-mono text-xs"
+			class:outline={searchTags.has(tag)}
+			value={tag}
+			on:click={() => handleTagClick(tag)}>&num;{tag}</button
+		>
 	{/each}
 </div>
 
-<section class="grid grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr">
+<section class="grid auto-rows-fr grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-3">
 	{#each content as app}
 		<Card {app} />
 	{/each}
